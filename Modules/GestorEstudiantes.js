feac5605a5f76d6f58c4a8474c48a15276ca7541
Kevin Moreno <jeankevinmoreno24@gmail.com>
2025-03-10 23:19:53 -0400

    funcion listar y actualizar

--- Modules/GestorEstudiantes.js
+++ Modules/GestorEstudiantes.js
@@ -7,5 +7,19 @@ export class GestorEstudiantes {
       const id = this.estudiantes.length + 1;
       this.estudiantes.push({ id, nombre, edad, nivel, calificaciones });
     }
+    listar() {
+        return this.estudiantes;
+      }
+    
+      actualizar(id, nombre, edad, nivel) {
+        for (let i = 0; i < this.estudiantes.length; i++) {
+          if (this.estudiantes[i].id === id) {
+            this.estudiantes[i] = { id, nombre, edad, nivel };
+            return true;
+          }
+        }
+        return false;
+      }
+    
 
 }
